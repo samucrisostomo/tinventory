c1b94f39176f765aefe44b62ea3879700c384b44
dev@cosampa.com.br <samolivercris@gmail.com>
2026-04-29 10:42:06 -0300

    Revert "Removendo welcome"
    This reverts commit 3ac080dfdb2496b04fd7f4253410e6a6a0c95e91.

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use App\Http\Controllers\TipoMaterialController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/', '/dashboard');
+Route::inertia('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'geral/dashboard/dashboard')->name('dashboard');
