3ac080dfdb2496b04fd7f4253410e6a6a0c95e91
dev@cosampa.com.br <samolivercris@gmail.com>
2026-04-29 10:34:43 -0300

    Removendo welcome

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use App\Http\Controllers\TipoMaterialController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::inertia('/', 'welcome')->name('home');
+Route::redirect('/', '/dashboard');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'geral/dashboard/dashboard')->name('dashboard');
